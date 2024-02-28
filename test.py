34fdd5afc5542459148ce4bcc2c23ed26a1213d2
Kousha <150776406+KoushaZhiyani@users.noreply.github.com>
2024-02-28 20:29:40 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 # NBMultinomial
+
 data_train = pd.read_csv(r"Training.csv")
 
 X = data_train.drop("prognosis", axis=1)
 y = data_train["prognosis"]
-# print(X)
-# input()
+
 test_data = pd.read_csv(r"Testing.csv")
 X_test = test_data.drop("prognosis", axis=1)
 y_test = test_data["prognosis"]
@@ -20,6 +20,7 @@ print(point)
 
 X = data.drop("v1", axis=1)
 y = data["v1"]
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.0018, random_state=40552, stratify=y)
 lab = label_encoder()
 lab.train(X_train)
